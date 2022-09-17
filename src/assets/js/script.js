835c96ad47eeab674d313e8081564d8d2e1ba9a0
DESKTOP-3TH5724\baske <basket100713hiroki.ch@gmail.com>
2022-09-17 18:31:04 +0900

    download-page完成

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -55,3 +55,30 @@ $(".service__question-card").on("click", function(){
   $(this).find(".service__question-card-answer").slideToggle(500);//回答文が現れる
 })
 
+
+//お問い合わせ完了メッセージ
+$(document).ready(function () {
+
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdKls0LPq9U7Q1I2Ef3eYspg8HJlMCEApTPtbquv1wd-380wg/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".download__page__apply__form__afterSend").slideDown();
+          $(".c-form__wrapper").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        // 200: function () {
+        //   $(".false-message").slideDown();
+        // }
+      }
+    });
+    event.preventDefault();
+  });
+
+});
+
